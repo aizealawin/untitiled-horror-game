41a7097a7476a15d8e1fceb4450dd562a8816168
aizealawin <ktmudalige@gmail.com>
2025-09-11 13:11:52 -0400

    creating baseline vars

--- Scripts/StarterScript.cs
+++ Scripts/StarterScript.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class StarterScript : MonoBehaviour
 {
     [SerializeField] int myNumber;
+    [SerializeField] string myName;
+    [SerializeField] bool myChoice;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
