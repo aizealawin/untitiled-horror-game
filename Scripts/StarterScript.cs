97145e0841450e7f4f1acf4498029b87cd93c005
aizealawin <ktmudalige@gmail.com>
2025-09-11 13:12:57 -0400

    init game object for gate

--- Scripts/StarterScript.cs
+++ Scripts/StarterScript.cs
@@ -5,6 +5,10 @@ public class StarterScript : MonoBehaviour
     [SerializeField] int myNumber;
     [SerializeField] string myName;
     [SerializeField] bool myChoice;
+
+    int myOtherNumber;
+
+    [SerializeField] GameObject myGate;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
