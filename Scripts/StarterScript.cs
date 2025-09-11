f70a53de392a77bbc6a7380e27416514b2cb1314
aizealawin <ktmudalige@gmail.com>
2025-09-11 13:10:11 -0400

    testing serialized fields

--- Scripts/StarterScript.cs
+++ Scripts/StarterScript.cs
@@ -2,15 +2,16 @@ using UnityEngine;
 
 public class StarterScript : MonoBehaviour
 {
+    [SerializeField] int myNumber;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
