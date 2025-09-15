95a770771e899657dec5575cc1f76a25f22e0caa
aizealawin <ktmudalige@gmail.com>
2025-09-15 18:23:01 -0400

    testing TMP serialization

--- Scripts/StarterScript.cs
+++ Scripts/StarterScript.cs
@@ -9,6 +9,8 @@ public class StarterScript : MonoBehaviour
     int myOtherNumber;
 
     [SerializeField] GameObject myGate;
+    [SerializeField] GameObject name1;
+    [SerializeField] GameObject name2;
 
 
     void Start()
@@ -25,11 +27,16 @@ public class StarterScript : MonoBehaviour
         {
             myName = "Kusal";
             myGate.SetActive(true);
+            name1.GetComponent<TMPro.TMP_Text>().text = $"My Number: {myNumber} My Name: {myName}";
+            name2.GetComponent<TMPro.TMP_Text>().text = $"____________";
+
         }
         else
         {
             myName = "Not Kusal";
             myGate.SetActive(false);
+            name2.GetComponent<TMPro.TMP_Text>().text = $"{myName} is active.";
+            name1.GetComponent<TMPro.TMP_Text>().text = "I am stinky";
         }
     }
 }
