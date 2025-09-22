64edc8646a095d50c85cfc26990fc3f27c50485e
aizealawin <ktmudalige@gmail.com>
2025-09-22 12:30:49 -0400

    Built initial raycast

---
+++ Scripts/RayCasting.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class RayCasting : MonoBehaviour
+{
+
+    public static float distanceFromTarget;
+    [SerializeField] float toTarget;
+
+
+    void Update()
+    {
+        // 
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit))
+        {
+            toTarget = hit.distance;
+            distanceFromTarget = hit.distance;
+        }
+    }
+}
